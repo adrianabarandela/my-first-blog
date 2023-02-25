9548b141e961dae23872fd7b0e55966863987d7f
Adria <adrianafb87@gmail.com>
2023-02-25 17:30:12 +0100

    Mi aplicación Django Girls, primer commit

---
+++ edito_django_girls.py
@@ -0,0 +1,49 @@
+"""
+print('Hello, Django girls!')
+if 3 < 2:
+    print('Si')
+else:
+    print('No')
+"""
+"""
+name='Pepe'
+if name == 'Toni':
+    print('Hola Toni')
+elif name == 'Adri':
+    print('Hola Adri')
+else:
+    print('No sé quien eres')
+"""
+"""
+volume = 123
+if volume < 20:
+    print("It's kinda quiet.")
+elif 20 <= volume < 40:
+    print("It's nice for background music")
+elif 40 <= volume < 60:
+    print("Perfect, I can hear all the details")
+elif 60 <= volume < 80:
+    print("Nice for parties")
+elif 80 <= volume < 100:
+    print("A bit loud!")
+else:
+    print("Me duelen las orejas! :(")
+"""
+"""
+def culo():
+    print('Me duele el culo de estar sentada')
+    print('Y a ti?')
+
+culo()
+"""
+"""
+def hi(name):
+    print('Hola ' + name)
+mujeres =['Adriana' , 'Raquel' , 'Josefa' , 'Julia' , 'Antonia']
+for name in mujeres:
+    hi(name)
+    print("")
+"""
+for i in range(1,10):
+    print(i)
+    
